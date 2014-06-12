b99aa5d372e5dbc74fac7c1b65e3bd0344f8c501
hiuchida <hiuchida@beacon-it.co.jp>
2014-06-12 17:16:33 +0900

    ver 20140612

--- src/jp/hiuchida/java_crawler/ParseJar.java
+++ src/jp/hiuchida/java_crawler/ParseJar.java
@@ -1,7 +1,9 @@
 package jp.hiuchida.java_crawler;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -10,6 +12,7 @@ public class ParseJar {
     static JavaMap javaMap = JavaMap.singleton;
     static JavaMap.JavaInfo javaInfo;
     public static void execute(String filepath, String filename) {
+        HashSet<String> fqcnSet = new HashSet<>();
     	javaInfo = javaMap.new JavaInfo();
     	javaInfo.fileName = filename;
     	javaMap.map.put(filepath, javaInfo);
@@ -21,9 +24,13 @@ public class ParseJar {
 			    	name = name.substring(0, name.length() - 6);
 			    	name = name.replaceAll("/", ".");
 			    	log.trace("C " + name);
+			    	fqcnSet.add(name);
 			    	javaMap.fqcnSet.add(name);
 			    }
 			}
+	        StringArray fqcn = new StringArray(fqcnSet);
+	        Collections.sort(fqcn);
+	        fqcn.save("jarinfo/" + filename + ".txt");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
